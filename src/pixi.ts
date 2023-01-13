0062738b9c0a2330d1bc5fdafaed43d132f3125f
StevenSyllabus <115730470+StevenSyllabus@users.noreply.github.com>
2023-01-13 15:30:05 -0800

    1 file modified

--- src/pixi.ts
+++ src/pixi.ts
@@ -4,6 +4,7 @@ const pixiContainer = <any>document.getElementById(`app`);
 
 console.log(PIXI);
 console.log(`test`);
+console.log(`yes`);
 let app = new PIXI.Application({
   width: 500,
   height: 500,
